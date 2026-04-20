986d521985e5f2e3bd997d636550610d8763d6ae
sanket-thakkar-404 <sanketthakkar500@gmail.com>
2026-04-20 11:34:16 +0530

    Frontend change

--- frontend/src/features/api/api.js
+++ frontend/src/features/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://system-desgin-q41f.onrender.com/api",
   withCredentials: true
 })
 
