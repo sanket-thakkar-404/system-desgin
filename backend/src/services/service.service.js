fdf6a765adf28fbe973467bd95102210326f813f
Sanket Thakkar <sanketthakkar500@gmail.com>
2026-04-25 22:55:19 +0530

    Change date format to en-GB with time zone settings
    Updated date formatting to use en-GB locale with specified options.

--- backend/src/services/service.service.js
+++ backend/src/services/service.service.js
@@ -48,7 +48,16 @@ export const getServiceLogic = async () => {
 
 export const assignServiceLogic = async ({ clientId, clientName, services, assignedTo }) => {
 
-  const date = new Date().toLocaleString("en-IN");
+  const date = new Date().toLocaleString("en-GB", {
+  timeZone: "Asia/Kolkata",
+  day: "2-digit",
+  month: "2-digit",
+  year: "numeric",
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+  hour12: false
+});
 
   const tasks = services.map(async (service) => {
 
